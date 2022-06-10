e7a8e22ac1cdd1133bf62ca07e742f90c2965f3a
smb685 <sylvester.brandon25@gmail.com>
2022-06-10 00:45:29 -0500

    files added to lib and test added to the file

---
+++ __test__/Intern.test.js
@@ -0,0 +1,19 @@
+const Employee = require("../lib/Intern");
+
+test("Can set school via constructor", () => {
+    const testValue = "LSU";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.school()).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern"; 
+    const e = new Intern("Foo", 1, "test@test.com", "LSU")
+    expect(e.school).toBe(testValue);
+});
+
+test("Can get school via getSchool()",() => {
+    const testValue = "LSU";
+    const e = new Intern("Foo", 1, "test@test.com",testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
